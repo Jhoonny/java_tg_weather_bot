1fee53c20e83538c97f46dbd8266ba9ebc756734
Ion <i.lupasco@gmail.com>
2018-04-27 21:38:49 +0300

    rm token

--- src/main/java/Conf.java
+++ src/main/java/Conf.java
@@ -1,12 +1,9 @@
 public class Conf {
 
-<<<<<<< HEAD
-    private final static String TOKENBOT   = "YOUR TOKEN FROM TELEGRAM";
-=======
-    private final static String TOKENBOT   = "YOUR TOKEN FROM BOT";
->>>>>>> 54bd77bd3fee631678add97d81af41f3c8ee8983
-    private final static String NAMEBOT = "YOUR BOT NAME";
-    private final static String APPID   = "YOUR TOKEN FROM openweathermap.org";
+
+    private final static String TOKENBOT = "YOUR TOKEN FROM TELEGRAM";
+    private final static String NAMEBOT  = "YOUR BOT NAME";
+    private final static String APPID    = "YOUR TOKEN FROM openweathermap.org";
 
     public String getToken() { return TOKENBOT; }
 
