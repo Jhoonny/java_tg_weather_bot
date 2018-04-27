711159eda859c9f684e397f1737061af07b0e6fe
Johnny_L <i.lupasco@gmail.com>
2018-04-27 21:06:29 +0300

    rm tokens

--- src/main/java/Conf.java
+++ src/main/java/Conf.java
@@ -1,8 +1,8 @@
 public class Conf {
 
-    private final static String TOKENBOT   = "545066834:AAFIV3tLnqRXaCluDtH0urC49_3uyc0C1zU";
-    private final static String NAMEBOT = "Weather_from_OWM_bot";
-    private final static String APPID   = "b5461e30fcda77eb6dd0539012bcf449";
+    private final static String TOKENBOT   = "YOUR TOKEN FROM BOT";
+    private final static String NAMEBOT = "YOUR BOT NAME";
+    private final static String APPID   = "YOUR TOKEN FROM openweathermap.org";
 
     public String getToken() { return TOKENBOT; }
 
